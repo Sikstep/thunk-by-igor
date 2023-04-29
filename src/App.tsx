5368f56787919a6f7d3516355bf9a959d3466aa3
Sikstep <Sikstep1988@gmail.com>
2023-04-29 13:55:03 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useAppDispatch, useAppSelector} from "./hooks/hooks";
+import {Todos} from "./Todos";
+import {todolistApi} from "./api/todolistApi";
+import {log} from "util";
+import {getTodolistsTC} from "./reducers/TodolistReducer";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let todos = useAppSelector(state => state.todos)
+    const dispatch = useAppDispatch()
+
+
+    useEffect(() => {
+        // todolistApi.getTodolists()
+        //     .then((res)=>console.log(res.data))
+        dispatch(getTodolistsTC())
+    }, [])
+
+    return (
+        <div className="App">
+            {todos.map(el => {
+                return (
+                    <Todos
+                        key={el.id}
+                        todolistId={el.id}
+                        title={el.title}
+                        filter={el.filter}
+                    />
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
